42d0083f687745df5fdff667785b11a28e1fcc89
htmlgod <ivannevtrilov212@gmail.com>
2023-01-17 21:36:05 +0300

    tex report, cmake, boost::po, readme

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 ## TODO
 
-1. [ ] CMakeLists.txt
-2. [ ] boost program_options
+1. [x] CMakeLists.txt
+2. [x] boost program_options
 3. [ ] hash
 4. [ ] clipboard
 5. [ ] encryption(kuznechik?)
@@ -11,3 +11,6 @@
 7. [ ] CPACK
 8. [ ] Pass gen
 9. [ ] pass entropy check
+10. [ ] hunter explicit url for boost
+11. [ ] class vault for passwords
+12. [ ] static functions
---
+++ sources/passvault.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+
+#include <boost/program_options.hpp>
+
+namespace po = boost::program_options;
+
+auto main(int argc, char** argv) -> int {
+    po::options_description desc("Allowed options");
+
+    desc.add_options()
+        ("help", "produce help message")
+        ("get_pass", po::value<std::string>(), "get password for site/service")
+        ("check_pass", po::value<std::string>(), "check password strength")
+        ("gen_pass_and_add", po::value<std::string>(), "gen password for site/service")
+        ("gen_pass", "gen password")
+    ;
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);    
+
+    if (vm.count("help")) {
+        std::cout << desc << "\n";
+        return 0;
+    }
+    if (vm.count("get_pass")) {
+        std::cout << "get_pass" << "\n";
+        return 0;
+    }
+    if (vm.count("check_pass")) {
+        std::cout << "check_pass" << "\n";
+        return 0;
+    }
+    if (vm.count("gen_pass_and_add")) {
+        std::cout << "gen_pass_and_add" << "\n";
+        return 0;
+    }
+    if (vm.count("gen_pass")) {
+        std::cout << "gen_pass" << "\n";
+        return 0;
+    }
+
+    return 0;
+}
